dd8ac9e11d6e15713218f05b244e8e6ff350173f
Quentin Blondel <qb@volpy.com>
2022-06-18 08:39:44 +0200

    clean file

--- src/components/Convert.vue
+++ src/components/Convert.vue
@@ -14,7 +14,6 @@
       </div>
       <div class="form-group">
         <button id="convertBtn" class="btn btn-primary" @click="convert">Convert</button>
-        <button id="convertSseBtn" class="btn btn-primary" @click="convertSSE">Convert with SSE</button>
       </div>
 
     <div v-if="resultDisplay">
@@ -50,34 +49,6 @@ export default {
     };
   },
 	mounted() {
-    let sseClient = this.$sse.create({
-      url: 'http://localhost:8000/api/convert/sse/',
-      format: 'json'
-    });
-
-    // Catch any errors (ie. lost connections, etc.)
-    sseClient.on('error', (e) => {
-      console.error('lost connection or failed to parse!', e);
-
-      // If this error is due to an unexpected disconnection, EventSource will
-      // automatically attempt to reconnect indefinitely. You will _not_ need to
-      // re-add your handlers.
-    });
-
-    // Handle messages without a specific event
-    sseClient.on('message', this.handleSSEResult);
-
-
-    sseClient.connect()
-      .then(sse => {
-        // console.log('SSE client connected !');
-      })
-      .catch((err) => {
-        // When this error is caught, it means the initial connection to the
-        // events server failed.  No automatic attempts to reconnect will be made.
-        console.error('Failed to connect to server', err);
-				this.resultError({message: 'Failed to connect to server'});
-      });
   },
   methods: {
     convert() {
@@ -90,24 +61,6 @@ export default {
 					;
 			}
     },
-		convertSSE() {
-			if(this.validateInput()){
-				this.clickDisplay();
-				axios
-					.get(`http://localhost:8000/api/convert/sse/toroman/${this.integerStr}`)
-					.catch((err) => this.resultError(err))
-					;
-			}
-    },
-		handleSSEResult(res) {
-			if(res.errorCode === 0){
-				this.resultSuccess(res);
-			}
-			else{
-				this.resultError(res);
-			}
-    },
-
     resultSuccess(res) {
       this.resultData = res;
 			this.initDisplay();
@@ -138,14 +91,6 @@ export default {
 			this.loadDisplay = false;
       this.resultDisplay = true;
 		}
-  },
-	beforeDestroy() {
-    // Make sure to close the connection with the events server
-    // when the component is destroyed, or we'll have ghost connections!
-    sseClient.disconnect();
-
-    // Alternatively, we could have added the `sse: { cleanup: true }` option to our component,
-    // and the SSEManager would have automatically disconnected during beforeDestroy.
-  },
+  }
 };
 </script>
